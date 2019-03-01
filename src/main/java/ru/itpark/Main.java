987c4ae53d172c39e3b8da8e800fa1c3e7df2358
ilmir <luke3skystone@gmail.com>
2019-03-01 12:25:02 +0300

    git hub connected

--- src/main/java/ru/itpark/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itpark;
-
-public class Main {
-    public static void main(String[] args) {
-        bonusService bonusService = new bonusService();
-        int bonus = bonusService.calculate(5_000,49_000);
-        System.out.println(bonus);
-    }
-}
